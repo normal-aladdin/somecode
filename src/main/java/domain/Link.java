7160b3c3837222fb013baa462490495b55f40d00
Aladdin <aladdin.ding@yunzhihui.com>
2017-09-16 12:21:06 +0800

    Add reverse link

---
+++ src/main/java/domain/Link.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class Link {
+	private Node head;
+
+	public Node getHead() {
+		return head;
+	}
+
+	public void setHead(Node head) {
+		this.head = head;
+	}
+
+	public Link(Node head) {
+		this.head = head;
+	}
+}
